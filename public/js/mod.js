2308e98589497506a91ffca36c04baf2997709c5
valkyrienyanko <sebastianbelle074@gmail.com>
2019-11-22 18:52:17 -0600

    Update

--- public/js/mod.js
+++ public/js/mod.js
@@ -8,12 +8,45 @@ fetch(myRequest).then(function(response) {
     }
     return response.json()
 }).then(function(response) {
-    const paragraph = document.createElement('p')
-    const summary = document.createTextNode(response.summary)
-    paragraph.appendChild(summary)
+    const name = response.name
+    const summary = response.summary
+    const description = response.description
+    const author = response.author
+    const picture_url = response.picture_url
     
-    document.body.appendChild(paragraph)
+    if (name != null) {
+      const title = createElement('a', name)
+      title.style.fontSize = "xx-large"
+      title.href = 'https://www.nexusmods.com/newvegas/mods/' + modId + '?tab=files'
+    }
+    
+    if (picture_url != null) {
+      const img = document.createElement('img')
+      img.src = picture_url
+      img.width = '500'
+      document.body.appendChild(img)
+    }
+    
+    if (summary != null) {
+      createElement('p', summary)
+    }
+    
+    if (description != null) {
+      createElement('p', description)
+    }
+    
+    if (author != null) {
+      createElement('p', author)
+    }
 })
+
+function createElement(type, content) {
+    const theElement = document.createElement(type)
+    const theElementText = document.createTextNode(content)
+    theElement.appendChild(theElementText)
+    document.body.appendChild(theElement)
+    return theElement;
+}
 
 function redirectHome() {
   window.location.href = '/'
